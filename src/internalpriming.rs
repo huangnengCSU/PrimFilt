22a923203568b1d84b848e15fc6be338155f99fd
Neng Huang <neng@ds.dfci.harvard.edu>
2026-01-10 10:43:53 -0500

    add annotation option

--- src/internalpriming.rs
+++ src/internalpriming.rs
@@ -1,4 +1,5 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
+use bio::data_structures::interval_tree::IntervalTree;
 use log::info;
 
 use rust_htslib::bam;
@@ -6,10 +7,11 @@ use rust_htslib::bam::ext::BamRecordExtensions;
 use rust_htslib::bam::{HeaderView, Reader, Record};
 use rust_htslib::bam::Read;
 use rust_htslib::bam::record::Aux;
+use crate::annotation::query_gene_tree;
 
-
-pub fn read_bam(bam_path: &str, anno_introns: &HashMap<String, Vec<String>>, ref_seqs: &HashMap<String, Vec<u8>>, chr: &str, window_size: usize, fraction: f32) -> Vec<Record> {
+pub fn read_bam(bam_path: &str, chr_gene_tree: &IntervalTree<u32, String>, chr_gene_introns: &HashMap<String, HashSet<String>>, ref_seqs: &HashMap<String, Vec<u8>>, chr: &str, window_size: usize, fraction: f32) -> (Vec<Record>, Vec<Record>) {
     let mut out_records = Vec::new();
+    let mut discarded_records = Vec::new();
     let mut bam = bam::IndexedReader::from_path(bam_path).expect("Failed to open BAM file");
     // bam.set_threads(num_threads).expect("Failed to set BAM threads");
     let header = bam.header().to_owned();
@@ -66,7 +68,6 @@ pub fn read_bam(bam_path: &str, anno_introns: &HashMap<String, Vec<String>>, ref
         if win2_end > chr_len as i64 {
             win2_end = chr_len as i64;
         }
-        // TODO: exceed start or end position on the chromosome
         let win1_ref_seq = ref_seqs[chr][win1_start as usize..win1_end as usize].to_ascii_lowercase();
         let win2_ref_seq = ref_seqs[chr][win2_start as usize..win2_end as usize].to_ascii_lowercase();
         // Calculate fraction of A's in the windows
@@ -79,21 +80,55 @@ pub fn read_bam(bam_path: &str, anno_introns: &HashMap<String, Vec<String>>, ref
         let win2_t_count = win2_ref_seq.iter().filter(|&&c| c == 't' as u8).count();
         let win1_t_fraction = win1_t_count as f32 / window_size as f32;
         let win2_t_fraction = win2_t_count as f32 / window_size as f32;
-        // if record.qname() == b"" {
+
+        // parse intron to see if the read has annotated intron
+        let mut pos = record.reference_start() as u32;  // 0-based
+        let mut read_introns: Vec<String> = Vec::new();
+        for cg in cigar.iter() {
+            let op = cg.char();
+            let len = cg.len() as u32;
+            if op == 'N' {
+                let intron_start = pos;
+                let intron_end = pos + len;
+                let intron_key = format!("{}:{}-{}", chr, intron_start + 1, intron_end); // 1-based, both inclusive
+                read_introns.push(intron_key);
+            }
+            if op == 'M' || op == '=' || op == 'X' || op == 'D' || op == 'N' {
+                pos += len;
+            }
+        }
+        let overlapping_genes: Vec<String> = query_gene_tree(&chr_gene_tree, start as u32 + 1, end as u32 + 1); // 1-based, start inclusive, end exclusive
+        let mut has_annotated_intron = false;
+        let mut annotated_intron = String::new();
+        for gene_name in overlapping_genes.iter() {
+            if !chr_gene_introns.contains_key(gene_name) {
+                continue;
+            }
+            let gene_introns = &chr_gene_introns[gene_name];
+            for intron in read_introns.iter() {
+                if gene_introns.contains(intron) {
+                    has_annotated_intron = true;
+                    annotated_intron = intron.clone();
+                    break;
+                }
+            }
+            if has_annotated_intron {
+                break;
+            }
+        }
+        // if record.qname() == b"<read_name>" {
         //     info!("{:?}\t{},{},{},{}", std::str::from_utf8(record.qname()), win1_a_fraction, win2_a_fraction, win1_t_fraction, win2_t_fraction);
         //     info!("igv region: {}:{}-{},{}:{}-{}", chr, win1_start + 1, win1_end, chr, win2_start + 1, win2_end);
         //     info!("ref pos (1-based, inclusive): {}\t{}", start + 1, end);
         //     info!("{:?}\t{:?}", std::str::from_utf8(&win1_ref_seq), std::str::from_utf8(&win2_ref_seq));
         //     info!("{}:{}, {}:{}", begin_cigar.unwrap().char(), begin_cigar.unwrap().len(), end_cigar.unwrap().char(), end_cigar.unwrap().len());
+        //     info!("Annotated intron: {}", annotated_intron);
         // }
-        if win1_a_fraction >= fraction || win1_t_fraction >= fraction || win2_a_fraction >= fraction || win2_t_fraction >= fraction {
-            // info!("{:?}\t{},{},{},{}", std::str::from_utf8(record.qname()), win1_a_fraction, win2_a_fraction, win1_t_fraction, win2_t_fraction);
-            continue;
+        if (win1_a_fraction >= fraction || win1_t_fraction >= fraction || win2_a_fraction >= fraction || win2_t_fraction >= fraction) && !has_annotated_intron {
+            discarded_records.push(record);
         } else {
-            // This read is not internally primed
-            // Handle accordingly (e.g., keep, log, etc.)
             out_records.push(record);
         }
     }
-    out_records
+    (out_records, discarded_records)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use clap::Parser;
 use rust_htslib::{bam, bam::Read};
 use std::time::Instant;
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
 use rayon::prelude::*;
 use std::sync::{Arc, Mutex};
@@ -14,9 +14,10 @@ use logs::init_logger;
 use log::info;
 
 mod annotation;
-use annotation::{load_junctions_from_annotation};
+use annotation::{load_gene_introns_from_annotation, build_gene_tree, query_gene_tree};
 
 use crate::internalpriming::read_bam;
+use crate::util::Region;
 
 mod internalpriming;
 
@@ -37,6 +38,10 @@ struct Args {
     /// Filtered output BAM file
     #[arg(short='o', long)]
     output: String,
+
+    /// Discarded output BAM file
+    #[arg(short='d', long)]
+    discarded_output: Option<String>,
 
     /// Window size for check interal priming
     #[arg(short='w', long, default_value_t = 20)]
@@ -62,6 +67,7 @@ fn main() {
     let reference_file = arg.reference.clone();
     let annotation_file = arg.annotation.clone();
     let output_bam_file = arg.output.clone();
+    let discarded_output_bam_file = arg.discarded_output.clone();
     let window_size = arg.window_size;
     let fraction = arg.fraction;
     let num_threads = arg.threads;
@@ -78,9 +84,10 @@ fn main() {
     info!("Fraction of A's: {}", fraction);
     info!("Number of threads: {}", num_threads);
 
-    let mut anno_intron_map = HashMap::new(); // key is junction chr:start-end, 1-based, both inclusive. value is a list of gene names containing this junction
+    let mut gene_introns: HashMap<String, HashMap<String, HashSet<String>>> = HashMap::new();
+    let mut gene_regions = HashMap::new();
     if annotation_file.is_some() {
-        anno_intron_map = load_junctions_from_annotation(annotation_file.unwrap().as_str());
+        (gene_introns, gene_regions) = load_gene_introns_from_annotation(annotation_file.unwrap().as_str());
     }
 
     let ref_seqs = load_reference(&reference_file);
@@ -89,25 +96,35 @@ fn main() {
     let header = bam::Header::from_template(bam.header());
     let fw: bam::Writer = bam::Writer::from_path(output_bam_file, &header, bam::Format::Bam).expect("Error opening output BAM file");
     let mutex_fw = Arc::new(Mutex::new(fw));
+    let mutex_discarded_fw = if let Some(discarded_file) = discarded_output_bam_file {
+        Some(Arc::new(Mutex::new(bam::Writer::from_path(discarded_file, &header, bam::Format::Bam).expect("Error opening discarded output BAM file"))))
+    } else {
+        None
+    };
 
     let pool = rayon::ThreadPoolBuilder::new().num_threads(num_threads).build().unwrap();
     pool.install(|| {
         info!("Thread pool with {} threads initialized.", num_threads);
         ref_seqs.par_iter().for_each(|(chr, seq)| {
-            let records = read_bam(&input_bam_file, &anno_intron_map, &ref_seqs, chr, window_size, fraction);
+            let empty_gene_regions: HashMap<String, Region> = HashMap::new();   // in case no providing annotation
+            let empty_gene_introns: HashMap<String, HashSet<String>> = HashMap::new();  // in case no providing annotation
+            let chr_gene_regions= gene_regions.get(chr).unwrap_or(&empty_gene_regions);
+            let chr_gene_introns = gene_introns.get(chr).unwrap_or(&empty_gene_introns);
+            let chr_gene_tree = build_gene_tree(&chr_gene_regions);
+            let (records, discarded_records) = read_bam(&input_bam_file, &chr_gene_tree, &chr_gene_introns, &ref_seqs, chr, window_size, fraction);
             info!("Number of records processed for chromosome {}: {}", chr, records.len());
             let mut writer = mutex_fw.lock().unwrap();
             for record in records {
                 writer.write(&record).expect("Error writing BAM record");
             }
+            if let Some(discarded_mutex) = &mutex_discarded_fw {
+                let mut discarded_writer = discarded_mutex.lock().unwrap();
+                for record in discarded_records {
+                    discarded_writer.write(&record).expect("Error writing discarded BAM record");
+                }
+            }
         });
     });
-
-    // for chr in ref_seqs.keys() {
-    //     // info!("Reference sequence loaded for chromosome: {}", chr);
-    //     let records = read_bam(&input_bam_file, &anno_intron_map, &ref_seqs, chr, window_size, fraction, num_threads);
-    //     info!("Number of records processed for chromosome {}: {}", chr, records.len());
-    // }
 
     let duration = start.elapsed();
     info!("Processing completed in: {:?}", duration);
